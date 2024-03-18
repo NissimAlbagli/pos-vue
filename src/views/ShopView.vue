47a706dcf63656f66cadeb890c9dda20794c22c1
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-18 20:37:02 -0300

    Se creo la barra de navegacion principal

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,10 +1,10 @@
 <script setup>
-
+    import MainNav from '../components/MainNav.vue';
 </script>
 
 <template>
     <div>
-        <h1>Shop Vue</h1>
+        <MainNav />
     </div>
 </template>
 
