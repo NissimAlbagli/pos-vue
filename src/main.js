d21589d005f5e874735ff6ef77cb26368240d198
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-19 17:51:08 -0300

    Se instalo y configuro firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from './config/firebase'
 import { plugin, defaultConfig } from '@formkit/vue';
 import config from '../formkit.config';
 
@@ -8,6 +10,11 @@ import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
+
+app.use(VueFire, {
+    firebaseApp,
+    modules: [VueFireAuth()],
+  })
 
 app.use(createPinia());
 app.use(plugin, defaultConfig(config));
