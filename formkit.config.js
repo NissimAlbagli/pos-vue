7153007ca3f733a2411b17389f62a042d748ce46
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-19 16:31:26 -0300

    Se agrego un archivo de configuracion para forkit y se configuro tailwind y main.js

---
+++ formkit.config.js
@@ -0,0 +1,16 @@
+import { generateClasses } from "@formkit/themes";
+
+const config = {
+    config: {
+        classes: generateClasses({
+            text: {
+                message: 'text-red-500'
+            },
+            number: {
+                message: 'text-red-500'
+            }
+        })
+    }
+}
+
+export default config
