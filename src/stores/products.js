5de00f5e42de1597ffd3e21b4da560b4b15d2d63
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-20 00:11:02 -0300

    Se agrego funcionalidad para editar productos

--- src/stores/products.js
+++ src/stores/products.js
@@ -1,7 +1,7 @@
 import { defineStore } from 'pinia';
 import { computed } from 'vue';
 import { useFirestore, useCollection } from 'vuefire';
-import { collection, addDoc, where, query, limit, orderBy } from 'firebase/firestore';
+import { collection, addDoc, where, query, limit, orderBy, updateDoc} from 'firebase/firestore';
 
 export const useProductsStore = defineStore('products', () => {
 
@@ -22,6 +22,19 @@ export const useProductsStore = defineStore('products', () => {
 
     async function createProduct(product) {
         await addDoc(collection(db, 'products'), product)
+    };
+
+    async function updateProduct(docRef, product) {
+        const { image, url, ...values } = product;
+
+        if(image.length) {
+            await updateDoc(docRef, {
+                ...values,
+                image: url.value
+            });
+        } else {
+            await updateDoc(docRef, values);
+        }
     }
 
     const categoryOptions = computed(() => {
@@ -33,12 +46,13 @@ export const useProductsStore = defineStore('products', () => {
         ]
 
         return options
-    })
+    });
 
     const noResults = computed(() => productsCollection.value.length === 0);
 
     return {
         createProduct,
+        updateProduct,
         productsCollection,
         categoryOptions,
         noResults
