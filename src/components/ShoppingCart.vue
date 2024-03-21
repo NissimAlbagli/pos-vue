215712a2b41d03cd9193e486493e2eb83e9ee37c
Nissim Albagli <nissimalbagli@gmail.com>
2024-03-21 19:32:08 -0300

    Se creo logica para calcular el subtotal

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,6 +1,8 @@
 <script setup>
     import { useCartStore } from '@/stores/cart';
+    import Amount from './Amount.vue'
     import ShoppingCartItem from './ShoppingCartItem.vue';
+    import { formatCurrency } from '@/helpers';
 
     const cart = useCartStore();
 </script>
@@ -20,5 +22,22 @@
                 :item="item"
             />
       </ul>
+
+      <dl class="space-y-6 border-t border-gray-200 pt-6 text-sm font-medium text-gray-500">
+        <Amount>
+            <template #label>Subtotal:</template>
+            {{ formatCurrency(cart.subtotal) }}
+        </Amount>
+
+        <Amount>
+            <template #label>Impuetos:</template>
+            {{ formatCurrency(0) }}
+        </Amount>
+
+        <Amount>
+            <template #label>Total a Pagar:</template>
+            {{ formatCurrency(0) }}
+        </Amount>
+      </dl>
     </div>
 </template>
--- src/stores/cart.js
+++ src/stores/cart.js
@@ -1,11 +1,18 @@
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
 import { defineStore } from "pinia";
 
 export const useCartStore = defineStore('cart', () => {
 
     const items = ref([]);
+    const subtotal = ref(0);
 
     const MAX_PRODUCTS = 5;
+
+    watch(items, () => {
+        subtotal.value = items.value.reduce((total, item) => total + (item.quantity * item.price), 0)
+    }, {
+        deep: true
+    })
 
     function addItem(item) {
         items.value.push({...item, quantity: 1, id: item.id})
@@ -23,6 +30,7 @@ export const useCartStore = defineStore('cart', () => {
 
     return {
         items,
+        subtotal,
         addItem,
         isEmpty,
         checkProductAvailability,
